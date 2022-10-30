77cb91b69925108af1e93a60dcf9a616cc7189b4
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-10-30 11:07:04 +0000

    first commit

---
+++ day_one/os.py
@@ -0,0 +1,12 @@
+import os
+
+def disk_usage(path):
+    total = os.path.getsize(path)
+    if os.path.isdir(path):
+        for file in os.listdir(path):
+            childpath = os.path.join(path, file)
+            total += disk_usage(childpath)
+    print('{0:<7}'.format(total), path)
+    return total
+
+disk_usage('../lecture3')
