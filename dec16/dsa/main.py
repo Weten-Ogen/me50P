7bda21c50addc6051b6a2393c71f4cf16c8ce1c4
Weten-Ogen <alphaoware@gmail.com>
2022-12-22 10:44:23 +0000

    worked on the general python skills

---
+++ dec16/dsa/main.py
@@ -0,0 +1,26 @@
+# tuples
+tup = (1,3,4,5)
+print(tup)
+print(tup[1:3])
+
+# list/Array
+
+lis = [1,2,3,4,5,6]
+print(lis)
+print(lis[2:4])
+
+# Dictionaries / hash tables
+
+dick = {
+    'name': 'Marcus',
+    'city': 'ksi',
+    'tel': 504493483
+}
+print(dick.keys())
+
+# Sets / immutable /unindexed /don't allow duplicates
+
+math = {1,1,4,5,7,7,8,9,0}
+math.update({10})
+
+print(math)
---
+++ dec16/dsa/stc.py
@@ -0,0 +1,20 @@
+# stack = []
+# stack.append(2)
+# stack.append(3)
+# stack.append(5)
+# print(stack)
+# stack.pop()
+# print(stack)
+
+# from queue import LifoQueue
+
+# stack = LifoQueue(maxsize=4)
+# stack.put(4)
+# stack.put(5)
+# stack.put(15)
+# stack.put(5)
+
+# print(stack.full())
+
+b =1 % 3
+print(b)
